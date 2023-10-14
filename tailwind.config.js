38e26bc3f0cb2e55201ca77afd98af0a41b3f319
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-10-14 20:44:59 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,16 +11,19 @@ module.exports = {
       current: "currentColor",
       dark: "#002f34",
       olx: "#0079fe",
-      headerBg:"#f7f8f8",
-      white :"#ffffff",
-      input:"#00e9df",
+      headerBg: "#f7f8f8",
+      white: "#ffffff",
+      input: "#00e9df",
     },
     extend: {
+      fontFamily: {
+        customFont: ["Roboto", "Nunito", "sans-serif"],
+      },
       padding: {
-        '100': '450px',
+        100: "450px",
       },
       height: {
-        '47px': '47px',
+        "47px": "47px",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
