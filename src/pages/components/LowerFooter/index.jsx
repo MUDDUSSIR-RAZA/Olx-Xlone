9e8244eaee9e3b4c2c471f71552bfe2306ca1b77
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-10-15 18:58:20 +0500

    Create index.jsx

---
+++ src/pages/components/LowerFooter/index.jsx
@@ -0,0 +1,14 @@
+const LowerFooter = () => {
+  return (
+    <>
+      <div className="w-full h-12 bg-dark flex items-center">
+        <div className="w-full" >
+          <div className="w-11/12 mx-auto flex justify-end text-white text-xs">
+            <span className=" font-semibold mr-2">Free Classifieds in Pakistan.</span> © 2006-2023 OLX
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default LowerFooter;
