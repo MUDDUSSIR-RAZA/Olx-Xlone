d7aa50148d1e835a2c613f5d89d4c7d073ab1819
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-10-16 17:03:10 +0500

    Create index.jsx

---
+++ src/pages/combinedComponents/FullFooter/index.jsx
@@ -0,0 +1,12 @@
+import LowerFooter from "@/pages/components/LowerFooter";
+import UpperFooter from "@/pages/components/UpperFooter";
+
+const FullFooter = () => {
+    return (
+        <>
+        <UpperFooter/>
+        <LowerFooter/>
+        </>
+    )
+}
+export default FullFooter;
