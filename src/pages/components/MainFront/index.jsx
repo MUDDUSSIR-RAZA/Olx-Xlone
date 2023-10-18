359d7459a7aa331dd694b5ce39a72c02b3729d99
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-10-18 15:16:55 +0500

    Update index.jsx

--- src/pages/components/MainFront/index.jsx
+++ src/pages/components/MainFront/index.jsx
@@ -2,37 +2,60 @@ import Image from "next/image";
 import Link from "next/link";
 
 const MainFront = () => {
-  const list = {
-    category: "Mobiles",
-    items: [
-      {
-        category: "Tablets",
-        adDetail: {
-          Title: "sdvfdsfdsf",
-          Description: "ahsjkhdsafdshfs",
-          Brand: "dsfdsf",
-          Condition: "sdfsfd",
-        },
-        price: "dfdsf",
-        image: "",
-        location: "",
-        userDetail: {
-          userName: "asfdfds",
-          number: "dsfdsf",
-        },
-      },
-    ],
-  };
+  // const list = [
+  //   {
+  //     category: "Mobiles",
+  //     items: [
+  //       {user : {},
+  //         category: "Tablets",
+  //         adTitle: "sdvfdsfdsf",
+  //         adDescription: "ahsjkhdsafdshfs",
+  //         adBrand: "dsfdsf",
+  //         adCondition: "sdfsfd",
+  //         price: "dfdsf",
+  //         image: "",
+  //         location: "",
+  //         userName: "asfdfds",
+  //         number: "dsfdsf",
+  //       },
+  //     ],
+  //   },
+  // ];
+
+  const list = [
+    {
+      "id": "0689f53a-f384-444a-a01a-8770ed180385",
+      "firstName": "MUHAMMAD MUDDUSSIR",
+      "lastName": "RAZA",
+      "email": "muddussir@gmail.com",
+      "password": "$2a$12$.4mQ1GoWJCGyL1YfKRgw/ucF.Ne2Ugjx41VfJxmE5Ja96w/P/X8Vm",
+      "number" : "03222665645",
+      "posts": [
+        {
+          "category": "Tablets",
+          "adTitle": "ipad",
+          "adDescription": "kch nahi",
+          "adBrand": "apple",
+          "adCondition": "good",
+          "price": "Rs 100,000",
+          "image": "/olxMobileApp.webp",
+          "location": "Majeed SRE Karachi",
+        }
+      ],
+      "picture": "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=626&ext=jpg"
+    }
+  ]
   return (
     <>
       <div className="w-full">
-        <div className="w-87% h-96 mx-auto my-auto ">
+        <div className="w-87% h-96 mx-auto my-auto mb-5">
           <div className="flex justify-between mb-4">
             <div className="text-2xl font-semibold">Mobile Phones</div>
-            <Link href={"/"} className="text-lightBlue flex font-semibold items-center">
-              <div>
-                 View more
-              </div>
+            <Link
+              href={"/"}
+              className="text-lightBlue flex font-semibold items-center"
+            >
+              <div>View more</div>
               <div className="fill-lightBlue -rotate-90 ml-2">
                 <Image
                   alt="iconArrowDown"
@@ -158,6 +181,7 @@ const MainFront = () => {
             </div>
           </div>
         </div>
+        
       </div>
     </>
   );
