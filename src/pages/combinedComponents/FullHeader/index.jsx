8f624841a44a474a45943217fb1fbad38a5349ae
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-10-19 17:56:40 +0500

    Update index.jsx

--- src/pages/combinedComponents/FullHeader/index.jsx
+++ src/pages/combinedComponents/FullHeader/index.jsx
@@ -5,11 +5,15 @@ import UpperHeader from "@/pages/components/UpperHeader";
 const FullHeader = () => {
   return (
     <>
-    <div className="fixed top-0 left-0 right-0 w-full z-50 bg-white">
-      <UpperHeader />
-      <LowerHeader />
-      <CategoriesHeader />
-    </div>
+      <div>
+        <div className="fixed top-0 left-0 right-0 w-full z-50 bg-white">
+          <UpperHeader />
+          <LowerHeader />
+        </div>
+        <div className="mt-32">
+          <CategoriesHeader />
+        </div>
+      </div>
     </>
   );
 };
