e49c8414e1e36ea0152f87aa7939afea62c0753a
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-10-19 18:19:38 +0500

    Update index.jsx

--- src/pages/components/MainFront/index.jsx
+++ src/pages/components/MainFront/index.jsx
@@ -62,7 +62,8 @@ const MainFront = () => {
           location: "Majeed SRE Karachi",
         },
       ],
-    }, {
+    },
+    {
       category: "Cars",
       posts: [
         {
@@ -121,7 +122,8 @@ const MainFront = () => {
           location: "Majeed SRE Karachi",
         },
       ],
-    }, {
+    },
+    {
       category: "Bikes & Motorcycles",
       posts: [
         {
@@ -180,7 +182,8 @@ const MainFront = () => {
           location: "Majeed SRE Karachi",
         },
       ],
-    }, {
+    },
+    {
       category: "Houses",
       posts: [
         {
@@ -239,7 +242,8 @@ const MainFront = () => {
           location: "Majeed SRE Karachi",
         },
       ],
-    }, {
+    },
+    {
       category: "TV - Video - Audio",
       posts: [
         {
@@ -298,7 +302,8 @@ const MainFront = () => {
           location: "Majeed SRE Karachi",
         },
       ],
-    },{
+    },
+    {
       category: "Tablets",
       posts: [
         {
@@ -357,7 +362,8 @@ const MainFront = () => {
           location: "Majeed SRE Karachi",
         },
       ],
-    },{
+    },
+    {
       category: "Land & Plots",
       posts: [
         {
@@ -449,140 +455,143 @@ const MainFront = () => {
   return (
     <>
       <div className="w-full">
-        <div className="w-87% h-96 mx-auto my-auto mb-5">
-          <div className="flex justify-between mb-4">
-            <div className="text-2xl font-semibold">Mobile Phones</div>
-            <Link
-              href={"/"}
-              className="text-lightBlue flex font-semibold items-center"
-            >
-              <div>View more</div>
-              <div className="fill-lightBlue -rotate-90 ml-2">
-                <Image
-                  alt="iconArrowDown"
-                  src={"/iconArrowDown.svg"}
-                  width={"15"}
-                  height={"15"}
-                  className="text-lightBlue fill-lightBlue"
-                />
-              </div>
-            </Link>
-          </div>
-
-          <div className="flex justify-between">
-            <div className="w-80 h-80 flex flex-col justify-between border rounded-md">
-              <div className="border-b h-2/3">
-                <Image
-                  src={"/olxMobileApp.webp"}
-                  width={"288"}
-                  height={"100"}
-                  className="w-full h-full rounded-md "
-                />
-              </div>
-
-              <div className=" w-full h-3/4 flex items-center">
-                <div className=" w-11/12 h-5/6 my-auto mx-auto ">
-                  <div className="w-full h-2/4 flex flex-col justify-between">
-                    <div className="font-semibold text-dark text-sm tracking-wide">
-                      RS 18,000
-                    </div>
-                    <div className="text-lg text-dark">Oppo a5s 3/32</div>
+        {allPosts.map((p) => {
+          return (
+            <div className="w-87% h-96 mx-auto my-auto mb-5">
+              <div className="flex justify-between mb-4">
+                <div className="text-2xl font-semibold">{p.category}</div>
+                <Link
+                  href={"/"}
+                  className="text-lightBlue flex font-semibold items-center"
+                >
+                  <div>View more</div>
+                  <div className="fill-lightBlue -rotate-90 ml-2">
+                    <Image
+                      alt="iconArrowDown"
+                      src={"/iconArrowDown.svg"}
+                      width={"15"}
+                      height={"15"}
+                      className="text-lightBlue fill-lightBlue"
+                    />
                   </div>
-                  <div className="w-full h-2/4  flex flex-col justify-end text-text text-sm">
-                    <div>Others, Faisalabad</div>
-                    <div>1 day ago</div>
+                </Link>
+              </div>
+
+              <div className="flex justify-between">
+                <div className="w-80 h-80 flex flex-col justify-between border rounded-md">
+                  <div className="border-b h-2/3">
+                    <Image
+                      src={"/olxMobileApp.webp"}
+                      width={"288"}
+                      height={"100"}
+                      className="w-full h-full rounded-md "
+                    />
+                  </div>
+
+                  <div className=" w-full h-3/4 flex items-center">
+                    <div className=" w-11/12 h-5/6 my-auto mx-auto ">
+                      <div className="w-full h-2/4 flex flex-col justify-between">
+                        <div className="font-semibold text-dark text-sm tracking-wide">
+                          RS 18,000
+                        </div>
+                        <div className="text-lg text-dark">Oppo a5s 3/32</div>
+                      </div>
+                      <div className="w-full h-2/4  flex flex-col justify-end text-text text-sm">
+                        <div>Others, Faisalabad</div>
+                        <div>1 day ago</div>
+                      </div>
+                    </div>
+                    <div></div>
                   </div>
                 </div>
-                <div></div>
-              </div>
-            </div>
 
-            <div className="h-full w-7"></div>
+                <div className="h-full w-7"></div>
 
-            <div className="w-80 h-80 flex flex-col justify-between border rounded-md">
-              <div className="border-b h-2/3">
-                <Image
-                  src={"/olxMobileApp.webp"}
-                  width={"288"}
-                  height={"100"}
-                  className="w-full h-full rounded-md "
-                />
-              </div>
-
-              <div className=" w-full h-3/4 flex items-center">
-                <div className=" w-11/12 h-5/6 my-auto mx-auto ">
-                  <div className="w-full h-2/4 flex flex-col justify-between">
-                    <div className="font-semibold text-dark text-sm tracking-wide">
-                      RS 18,000
-                    </div>
-                    <div className="text-lg text-dark">Oppo a5s 3/32</div>
+                <div className="w-80 h-80 flex flex-col justify-between border rounded-md">
+                  <div className="border-b h-2/3">
+                    <Image
+                      src={"/olxMobileApp.webp"}
+                      width={"288"}
+                      height={"100"}
+                      className="w-full h-full rounded-md "
+                    />
                   </div>
-                  <div className="w-full h-2/4  flex flex-col justify-end text-text text-sm">
-                    <div>Others, Faisalabad</div>
-                    <div>1 day ago</div>
+
+                  <div className=" w-full h-3/4 flex items-center">
+                    <div className=" w-11/12 h-5/6 my-auto mx-auto ">
+                      <div className="w-full h-2/4 flex flex-col justify-between">
+                        <div className="font-semibold text-dark text-sm tracking-wide">
+                          RS 18,000
+                        </div>
+                        <div className="text-lg text-dark">Oppo a5s 3/32</div>
+                      </div>
+                      <div className="w-full h-2/4  flex flex-col justify-end text-text text-sm">
+                        <div>Others, Faisalabad</div>
+                        <div>1 day ago</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="h-full w-7"></div>
+
+                <div className="w-80 h-80 flex flex-col justify-between border rounded-md">
+                  <div className="border-b h-2/3">
+                    <Image
+                      src={"/olxMobileApp.webp"}
+                      width={"288"}
+                      height={"100"}
+                      className="w-full h-full rounded-md "
+                    />
+                  </div>
+
+                  <div className=" w-full h-3/4 flex items-center">
+                    <div className=" w-11/12 h-5/6 my-auto mx-auto ">
+                      <div className="w-full h-2/4 flex flex-col justify-between">
+                        <div className="font-semibold text-dark text-sm tracking-wide">
+                          RS 18,000
+                        </div>
+                        <div className="text-lg text-dark">Oppo a5s 3/32</div>
+                      </div>
+                      <div className="w-full h-2/4  flex flex-col justify-end text-text text-sm">
+                        <div>Others, Faisalabad</div>
+                        <div>1 day ago</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="h-full w-7"></div>
+
+                <div className="w-80 h-80 flex flex-col justify-between border rounded-md">
+                  <div className="border-b h-2/3">
+                    <Image
+                      src={"/olxMobileApp.webp"}
+                      width={"288"}
+                      height={"100"}
+                      className="w-full h-full rounded-md "
+                    />
+                  </div>
+
+                  <div className=" w-full h-3/4 flex items-center">
+                    <div className=" w-11/12 h-5/6 my-auto mx-auto ">
+                      <div className="w-full h-2/4 flex flex-col justify-between">
+                        <div className="font-semibold text-dark text-sm tracking-wide">
+                          RS 18,000
+                        </div>
+                        <div className="text-lg text-dark">Oppo a5s 3/32</div>
+                      </div>
+                      <div className="w-full h-2/4  flex flex-col justify-end text-text text-sm">
+                        <div>Others, Faisalabad</div>
+                        <div>1 day ago</div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-
-            <div className="h-full w-7"></div>
-
-            <div className="w-80 h-80 flex flex-col justify-between border rounded-md">
-              <div className="border-b h-2/3">
-                <Image
-                  src={"/olxMobileApp.webp"}
-                  width={"288"}
-                  height={"100"}
-                  className="w-full h-full rounded-md "
-                />
-              </div>
-
-              <div className=" w-full h-3/4 flex items-center">
-                <div className=" w-11/12 h-5/6 my-auto mx-auto ">
-                  <div className="w-full h-2/4 flex flex-col justify-between">
-                    <div className="font-semibold text-dark text-sm tracking-wide">
-                      RS 18,000
-                    </div>
-                    <div className="text-lg text-dark">Oppo a5s 3/32</div>
-                  </div>
-                  <div className="w-full h-2/4  flex flex-col justify-end text-text text-sm">
-                    <div>Others, Faisalabad</div>
-                    <div>1 day ago</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="h-full w-7"></div>
-
-            <div className="w-80 h-80 flex flex-col justify-between border rounded-md">
-              <div className="border-b h-2/3">
-                <Image
-                  src={"/olxMobileApp.webp"}
-                  width={"288"}
-                  height={"100"}
-                  className="w-full h-full rounded-md "
-                />
-              </div>
-
-              <div className=" w-full h-3/4 flex items-center">
-                <div className=" w-11/12 h-5/6 my-auto mx-auto ">
-                  <div className="w-full h-2/4 flex flex-col justify-between">
-                    <div className="font-semibold text-dark text-sm tracking-wide">
-                      RS 18,000
-                    </div>
-                    <div className="text-lg text-dark">Oppo a5s 3/32</div>
-                  </div>
-                  <div className="w-full h-2/4  flex flex-col justify-end text-text text-sm">
-                    <div>Others, Faisalabad</div>
-                    <div>1 day ago</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
+          );
+        })}
       </div>
     </>
   );
