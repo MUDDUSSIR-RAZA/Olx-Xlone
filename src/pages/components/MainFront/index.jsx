c7aee7bd0a63cd6ec039ac39aaaabb8890e1d5c4
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-10-19 18:22:02 +0500

    Update index.jsx

--- src/pages/components/MainFront/index.jsx
+++ src/pages/components/MainFront/index.jsx
@@ -509,32 +509,6 @@ const MainFront = () => {
 
                 <div className="h-full w-7"></div>
 
-                <div className="w-80 h-80 flex flex-col justify-between border rounded-md">
-                  <div className="border-b h-2/3">
-                    <Image
-                      src={"/olxMobileApp.webp"}
-                      width={"288"}
-                      height={"100"}
-                      className="w-full h-full rounded-md "
-                    />
-                  </div>
-
-                  <div className=" w-full h-3/4 flex items-center">
-                    <div className=" w-11/12 h-5/6 my-auto mx-auto ">
-                      <div className="w-full h-2/4 flex flex-col justify-between">
-                        <div className="font-semibold text-dark text-sm tracking-wide">
-                          RS 18,000
-                        </div>
-                        <div className="text-lg text-dark">Oppo a5s 3/32</div>
-                      </div>
-                      <div className="w-full h-2/4  flex flex-col justify-end text-text text-sm">
-                        <div>Others, Faisalabad</div>
-                        <div>1 day ago</div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
               </div>
             </div>
           );
