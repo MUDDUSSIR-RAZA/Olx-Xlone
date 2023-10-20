38f2ca3bb021ccca1be204dab698e48143d1d506
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-10-20 21:11:11 +0500

    Update index.jsx

--- src/pages/components/SwipeBanner/index.jsx
+++ src/pages/components/SwipeBanner/index.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 
@@ -7,7 +8,7 @@ function SwipeBanner() {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentSlide((prevSlide) => (prevSlide === 1 ? 2 : 1));
-    }, 3000); // Change the interval time (in milliseconds) as per your preference
+    }, 3000);
 
     return () => {
       clearInterval(interval);
@@ -22,18 +23,18 @@ function SwipeBanner() {
             display: "flex",
             width: "196vw",
             overflow: "hidden",
-            margin:"1vw 5vw",
+            margin: "2vw 8vw",
           }}
         >
           <div
             style={{
               width: "100%",
               transform: `translateX(-${(currentSlide - 1) * 100}%)`,
-              transition: "transform .5s ease-in-out",
+              transition: "transform .1s ease-in-out",
               display: "flex",
             }}
           >
-            <div>
+            <div className="rounded-lg">
               <Link
                 href="https://onelink.to/6p6zqr"
                 title="Sell it, Search it, Buy it"
@@ -41,7 +42,12 @@ function SwipeBanner() {
                 target="_blank"
               />
               <div>
-                <img src={"/swipeBanner.webp"} style={{ width: "90%" }} />
+                {/* <Image
+                  src={"/swipeBanner.webp"}
+                  width={"1280"}
+                  height={"180"}
+                /> */}
+                <img src={"/swipeBanner.webp"} style={{ width: "1280px" ,height :"180px" , borderRadius:"11px" }} />
               </div>
             </div>
           </div>
@@ -49,7 +55,7 @@ function SwipeBanner() {
             style={{
               width: "100%",
               transform: `translateX(-${(currentSlide - 1) * 100}%)`,
-              transition: "transform .5s ease-in-out",
+              transition: "transform .1s ease-in-out",
               display: "flex",
             }}
           >
@@ -61,7 +67,12 @@ function SwipeBanner() {
                 target="_blank"
               />
               <div>
-                <img src={"/swipeBanner.webp"} style={{ width: "90%"  }} />
+                <img src={"/swipeBanner.webp"} style={{ width: "1280px" ,height :"180px",  borderRadius:"11px" }} />
+                {/* <Image
+                  src={"/swipeBanner.webp"}
+                  width={"1280"}
+                  height={"100"}
+                /> */}
               </div>
             </div>
           </div>
