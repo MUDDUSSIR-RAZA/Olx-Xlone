d8c07a74ba6b54d555a726697c9885223c8fa75d
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-10-20 21:11:16 +0500

    Update index.jsx

--- src/pages/miniComponents/SecondSearch/index.jsx
+++ src/pages/miniComponents/SecondSearch/index.jsx
@@ -6,24 +6,21 @@ const SecondSearch = () => {
   }
   return (
     <>
-      <div className="flex items-center h-full w-96">
+      <div className="flex items-center h-full w-95%">
         {/* <div className="border-t-2 border-b-2 border-l-2 border-dark rounded-l p-2.5 pr-4 bg-white focus-within:border-input"> */}
         <div className="focus-within:border-input bg-white border-dark rounded-l border-t-2 border-b-2 border-l-2 h-full w-full">
-          <div aria-label="Search input " className="flex items-center">
-            <div>
-              <div className="h-full">
+          <div aria-label="Search input " className="flex items-center w-full h-full">
+              <div className="h-full w-full flex">
                 <input
-                  // type="search"
+                  type="search"
                   spellheck="false"
                   autoComplete="free-text-search"
-                  value=""
                   placeholder="Find Cars, Mobile Phones and more..."
-                  className="w-full focus:outline-none"
+                  className="ml-3 w-full h-full focus:outline-none flex-1"
                   onChange={handleInputChange}
                 />
               </div>
             </div>
-          </div>
         </div>
         <div className="display: flex justify-center w-12 h-47px bg-dark rounded-r">
           <button className="fill-white">
