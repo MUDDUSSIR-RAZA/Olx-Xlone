1eca80585cad2c4260e1678d69cd5ddce2966859
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-10-26 18:14:24 +0500

    Update index.jsx

--- src/pages/components/UpperHeader/index.jsx
+++ src/pages/components/UpperHeader/index.jsx
@@ -1,21 +1,21 @@
-import Property from "@/pages/icons/Property";
-import UpperHeaderOlx from "@/pages/icons/UpperHeaderOlx";
-import Motor from "@/pages/icons/Motors";
-import { Inter } from "next/font/google";
-import Link from "next/link";
+// import Property from "@/pages/icons/Property";
+// import UpperHeaderOlx from "@/pages/icons/UpperHeaderOlx";
+// import Motor from "@/pages/icons/Motors";
+// import { Inter } from "next/font/google";
+// import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
-export default function UpperHeader() {
-  return (
-    <>
-      <div className="flex items-center pl-24 pt-3 bg-headerBg">
-        <UpperHeaderOlx />
+// export default function UpperHeader() {
+//   return (
+//     <>
+//       <div className="flex items-center pl-24 pt-3 bg-headerBg">
+//         <UpperHeaderOlx />
 
-        <Motor />
+//         <Motor />
 
-        <Property />
-      </div>
-    </>
-  );
-}
+//         <Property />
+//       </div>
+//     </>
+//   );
+// }
